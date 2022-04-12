659580045967105dae4dd65b83995d89551dbc6c
Davide Longo <79080322+pr-davidelongo@users.noreply.github.com>
2022-04-12 17:36:18 +0200

    v.01.000.000

--- public_html/js/crypto-com.js
+++ public_html/js/crypto-com.js
@@ -1,18 +1,38 @@
+var userCoins,jsonLenght; // Object.keys(coins).length; // restituisce dimensione JSON
+
 function currency(value) {
     new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value);
-};
-var userCoins;
+}
 
-function getUserCoins(user) {
+function getUserCoinsTable(user) {
     $.ajax({
         url: `http://localhost:3000/coins`,
         type: 'POST',
+        dataType: 'json',
         success: function(coins) {
-            userCoins = JSON.parse(coins);
+            jsonLenght=Object.keys(coins).length; 
+            for (let id = 1; id < Object.keys(coins).length/*restituisce dimensione JSON*/; id++) {
+                console.log(coins[id]);
+                if (id > 1) {
+                    $("#tbCoin").append(
+                        `<tr id="coin${coins[id].coin}">
+                            <td class="trID">${id}</td>
+                            <td class="trLOGO"><img src="media/icon/${coins[id].coin}.png" alt="${coins[id].coin} Logo" width="32px" height="32px"></td>
+                            <td class="trCOIN">${coins[id].coin}</td>
+                            <td class="trVALUE">152.35</td>
+                        </tr>`);
+                } else {
+                    $("#tbCoin").html(
+                        `<tr id="coin${coins[id].coin}">
+                            <td class="trID">${id}</td>
+                            <td class="trLOGO"><img src="media/icon/${coins[id].coin}.png" alt="${coins[id].coin} Logo" width="32px" height="32px"></td>
+                            <td class="trCOIN">${coins[id].coin}</td>
+                            <td class="trVALUE">152.35</td>
+                        </tr>`);
+                }
+            }
         },
-        error: function(error) {
-
-        }
+        error: function(error) {}
     });
 }
 
@@ -51,50 +71,11 @@ function getRowTicker(coin1, coin2, id) {
     });
 }
 
-function makeTableCoin() {
-
-    for (let c = 1; c < userCoins.lenght; c++) {
-        console.log(array[c]);
-
-    }
-    /* if (id > 1) {
-        $("#tbCoin").append(
-            `<tr id="coin${coin1}">
-                        <td class="trID">${id}</td>
-                        <td class="trLOGO"><img src="media/icon/${coin1}.png" alt="${coin1} Logo" width="32px" height="32px"></td>
-                        <td class="trCOIN">${coinRead}</td>
-                        <td class="trVALUE">${coinValue}</td>
-                    </tr>`);
-    } else {
-        $("#tbCoin").html(
-            `<tr id="coin${coin1}">
-                        <td class="trID">${id}</td>
-                        <td class="trLOGO"><img src="media/icon/${coin1}.png" alt="${coin1} Logo" width="32px" height="32px"></td>
-                        <td class="trCOIN">${coinRead}</td>
-                        <td class="trVALUE">${coinValue} $</td>
-                    </tr>`);
-    } */
-
-
-
-    /* getRowTicker('BTC', 'USDT', 1);
-    
-        getRowTicker('ETH', 'USDT', 2);
-        getRowTicker('CRO', 'USDT', 3);
-        getRowTicker('LUNA', 'USDT', 4);
-        getRowTicker('STX', 'USDT', 5); */
-
-}
-
-
-
-
-
 
 $(document).ready(function() {
     console.log("sono pronto!")
         // options for GET
-    getUserCoins();
+        getUserCoinsTable();
 
-    makeTableCoin();
+    //makeTableCoin();
 });
