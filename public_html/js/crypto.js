15062e0d524bf9041919100f90fe5e044043b104
Davide Longo <davide@programmarerobot.it>
2022-04-21 17:27:05 +0200

    v01.002

--- public_html/js/crypto.js
+++ public_html/js/crypto.js
@@ -1,45 +1,83 @@
 const urlServer = 'http://localhost:3000/';
-var userCoins;
+var userCoins, coinPrice;
 
 function currency(value) {
     new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value);
 }
 
-function coinsTable(user){
+function getUserCoins(user){
     $.ajax({
-        url: urlServer+'coins',
+        url: urlServer+'coins/'+user,
         type:'POST',
         dataType:'JSON',
         success: function(coins){
-            userCoins = coins;
-            console.log(coins);
-
+            for (let id = 1; id < Object.keys(coins).length/*restituisce dimensione JSON*/; id++) {
+                if (id > 1) {
+                    $("#tbCoin").append(
+                        `<tr id="${coins[id].coin}">
+                            <td class="trID">${id}</td>
+                            <td class="trLOGO"><img src="media/icon/${coins[id].coin}.png" alt="${coins[id].coin} Logo" width="32px" height="32px"></td>
+                            <td class="trCOIN">${coins[id].coin}</td>
+                            <td class="trQTY">${coins[id].qty}</td>
+                            <td class="trVALUE">${coins[id].mvalue}</td>
+                            <td class="trPRICE" id="${coins[id].coin}_${coins[0].coin}"></td>
+                        </tr>`);
+                } else {
+                    $("#tbCoin").html(
+                        `<tr id="${coins[id].coin}">
+                            <td class="trID">${id}</td>
+                            <td class="trLOGO"><img src="media/icon/${coins[id].coin}.png" alt="${coins[id].coin} Logo" width="32px" height="32px"></td>
+                            <td class="trCOIN">${coins[id].coin}</td>
+                            <td class="trQTY">${coins[id].qty}</td>
+                            <td class="trVALUE">${coins[id].mvalue}</td>
+                            <td class="trPRICE" id="${coins[id].coin}_${coins[0].coin}"></td>
+                        </tr>`);
+                }
+                //getPriceUSDT(coins[id].coin+'_'+coins[0].coin);
+                setInterval(function() {getPriceUSDT(coins[id].coin+'_'+coins[0].coin)}, 500);
+                
+            }
         },error: function(err){}
     })
 }
 
-function coinsPrice(user){
+function getPriceUSDT(coin){
+
     $.ajax({
-        url: urlServer+'coins/prices-'+user,
+        url: urlServer+'price/'+coin,
         type:'POST',
         dataType:'JSON',
-        success: function(coins){
-            userCoins = coins;
-            console.log(coins);
-
+        success: function(ticker){
+            if(ticker.result.data.b == undefined){
+                $(`#${coin}`).html(1.00)
+            }else{$(`#${coin}`).html(ticker.result.data.b)}
+            
+                
         },error: function(err){}
     })
+
+    /* for (let id = 1; id < Object.keys(coinsUser).length; id++) {
+        if(ticker == undefined){
+            coinsJSON[id].price = 1.00
+        }else{
+            coinsJSON[id].price = ticker; //salvo il valore della coin
+        }
+        console.log(coinsJSON[id].price);
+    } */
 }
 
+function saveUserCoin(user){
+    const myInterval = setInterval(function() {getUserCoins(user);console.log(userCoins)}, 1000);
 
+}
 
 
 $(document).ready(function() {
     console.log("sono pronto!")
 
     // options for GET
-    coinsPrice('coins');
-    setInterval(function() {console.log(userCoins)}, 5000);
-
-    ;
+    getUserCoins('coins');
+    //setInterval(function() {getUserCoins('coins');console.log(userCoins)}, 1000);
+    //getPriceUSDT('BTC_USDT');
+    //setInterval(function() {getUserCoins('coins');console.log(userCoins)}, 1000);
 });
--- server.js
+++ server.js
@@ -2,53 +2,75 @@ const https = require('https');
 const fs = require('fs');
 const cors = require('cors');
 const express = require('express');
+const { handle } = require('express/lib/application');
 const app = express();
+
+function getTicker(coin){
+    var options ={
+        host: 'api.crypto.com', // here only the domain name (no http/https!)
+        path: `/v2/public/get-ticker?instrument_name=${coin}`,
+        method: 'GET' // do POST
+    }; 
+
+    const coinVal = https.request(options, (coinRes) => { //chiedo dati all'API nell'opzions
+        coinRes.on('data', (d) => {
+            resJSON = JSON.parse(d); // parsifico la lettura      ret = resJSON.result.data.b
+        });
+        coinRes.on('end',()=>{
+            //console.log(resJSON.result.data) // valore coin
+            //return(resJSON.result.data);
+        });
+    });
+    coinVal.on('error', (error) => {throw error;});
+    coinVal.end(); // chiudo la richiesta all'API
+
+}
+
 
 app.use(express.static('public_html'));
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:3000'], methods: ['GET', 'POST'] }));
+app.use(cors({ origin: ['http://localhost:3000'], methods: ['GET', 'POST'] }))
 
+app.post('/price/:coin', (req, res) => {
+    var options ={
+        host: 'api.crypto.com', // here only the domain name (no http/https!)
+        path: `/v2/public/get-ticker?instrument_name=${req.params.coin}`,
+        method: 'GET' // do POST
+    }; 
 
-
-app.post('/coins', (req, res) => {
-
-    var coinsUser = require('./db/coins.json');
-    console.log(coinsUser);
-    res.send(coinsUser);
-});
-app.post('/coins/prices-:usr', (req, res) => {
-
-    var coinsUser = require(`./db/${req.params.usr}.json`);
-    //console.log(coinsUser);
-    //res.send(coinsUser);
-
-    for (let id = 1; id < Object.keys(coinsUser).length; id++) {
-        var coinsJSON;
-        console.log(id);
-        /* var options ={
-            host: 'api.crypto.com', // here only the domain name (no http/https!)
-            path: `/v2/public/get-ticker?instrument_name=${coinsUser[id].coin}_${coinsUser[0].coin}`,
-            method: 'GET' // do POST
-        }; 
-        const coinVal = https.request(options, coinRes => { //chiedo dati all'API nell'opzions
-            coinRes.on('data', d => {
-                resJSON = JSON.parse(d); // parsifico la lettura
-                if(resJSON.result.data.b == undefined){
-                    coinsJSON[id].price = 1.00
-                }else{
-                    coinsJSON[id].price = resJSON.result.data.b; //salvo il valore della coin
-                }
-                console.log(coinsJSON[id].price);
-            });
+    const coinVal = https.request(options, (coinRes) => { //chiedo dati all'API nell'opzions
+        coinRes.on('data', (d) => {
+            resJSON = JSON.parse(d); // parsifico la lettura      ret = resJSON.result.data.b;
+            res.send(resJSON);
         });
-        coinVal.on('error', error => {console.error(error)});
-        coinVal.end(); // chiudo la richiesta all'API */
-    }
-    res.send(coinsJSON);
+        //coinRes.on('end',()=>{
+            //console.log(resJSON.result.data) // valore coin
+            
+        //});
+    });
+    coinVal.on('error', (error) => {throw error;});
+    coinVal.end(); // chiudo la richiesta all'API
+})
+
+
+app.post('/coins/:usr', (req, res) => {
+
+    coinsUser = require(`./db/${req.params.usr}.json`);
+
+    /* for (let id = 1; id < Object.keys(coinsUser).length; id++) {
+        if(ticker == undefined){
+            coinsJSON[id].price = 1.00
+        }else{
+            coinsJSON[id].price = ticker; //salvo il valore della coin
+        }
+        console.log(coinsJSON[id].price);
+    } */
+
+    res.send(coinsUser);
 });
 
 
 
 app.get('/', (req, res) => {res.sendFile('index.html', { root: __dirname + "/public_html" })});
 app.all('*', (req, res) => {res.sendFile('404.html', { root: __dirname + "/public_html" })});
-app.listen(3000)
+app.listen(3000);
